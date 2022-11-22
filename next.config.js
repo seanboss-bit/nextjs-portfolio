876b691a07468e4659c5d5899554e4057af30bfd
Abubakar Musa <shaunaibek@gmail.com>
2022-11-22 09:43:01 +0100

    first pwa with next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'images.unsplash.com'
-    ]
-  }
-}
+    domains: ["images.unsplash.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="manifest" href="/mainfest.json" />
       </Head>
       <body>
         <Main />
